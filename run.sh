dfc7467b54d57c4452516293a330a7eb4f41b15b
Nirmal Gupta <nirmalk.gupta@gmail.com>
2017-07-16 13:29:28 -0700

    for static sites.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #echo UPSTREAM_SERVER is $UPSTREAM_SERVER
 #echo COINPIT_INT_IP is $COINPIT_INT_IP
-[ ! -f /etc/nginx/dhparams.pem ] && openssl dhparam -out /etc/nginx/dhparams.pem 2048
+#[ ! -f /etc/nginx/dhparams.pem ] && openssl dhparam -out /etc/nginx/dhparams.pem 2048
 #echo "$COINPIT_INT_IP     coinpitapp" >> /etc/hosts
 eval echo "\"$(cat <<EOF
 $(<site-https.in )
